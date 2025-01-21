966f38cf6d6e7b24ed6d93f83faba745a29cec39
verdic <verdict.gonzales@gmail.com>
2025-01-21 06:04:52 +0000

    initializing localweb push

--- bamboohayan/settings.py
+++ bamboohayan/settings.py
@@ -150,6 +150,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
